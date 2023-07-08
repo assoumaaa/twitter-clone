d8f0017b6550cc603c63d38992a9738d5a5ad60b
Omar Assouma <assouma@sabanciuniv.edu>
2023-07-09 00:51:20 +0300

    display posts

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,32 @@
+import { User } from "@clerk/nextjs/dist/types/server";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from " /server/api/trpc";
+import { clerkClient } from "@clerk/nextjs";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.profileImageUrl,
+  };
+};
+
+export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
+  }),
+});
